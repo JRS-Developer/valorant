d75ffbacdde8a9c72e8638194a65dd3305377605
Jose Sanchez <sanchezjoserafael25@gmail.com>
2021-03-06 22:03:06 -0600

    Avanzado el responsive, falta algunos detalles pero ah habido una gran mejora.

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,3 +1,5 @@
+// Funcion para cambiar el video
+
 var video = document.getElementById('video');
 
 
@@ -7,24 +9,38 @@ var Tailwind = document.getElementById('tailwind')
 var BladeStorm = document.getElementById('blade-storm')
 
 const changeVideoCloudburst = Cloudburst.addEventListener('click', () => {
-    if (video.getAttribute('src') !== "/videos/Jett_C_v001_web.mp4"){
-        video.setAttribute('src','/videos/Jett_C_v001_web.mp4')
+    if (video.getAttribute('src') !== "/assets/videos/Jett_C_v001_web.mp4"){
+        video.setAttribute('src','/assets/videos/Jett_C_v001_web.mp4')
     }
     
 })
 
 const changeVideoUpdraft = Updraft.addEventListener('click', () => {
-    if (video.getAttribute('src') !== "/videos/Jett_Q_v001_web.mp4"){
-        video.setAttribute('src','/videos/Jett_Q_v001_web.mp4')
+    if (video.getAttribute('src') !== "/assets/videos/Jett_Q_v001_web.mp4"){
+        video.setAttribute('src','/assets/videos/Jett_Q_v001_web.mp4')
     }
 })
 const changeVideoTailwind = Tailwind.addEventListener('click', () => {
-    if (video.getAttribute('src') !== "/videos/Jett_E_v001_web.mp4"){
-        video.setAttribute('src','/videos/Jett_E_v001_web.mp4')
+    if (video.getAttribute('src') !== "/assets/videos/Jett_E_v001_web.mp4"){
+        video.setAttribute('src','/assets/videos/Jett_E_v001_web.mp4')
     }
 })
 const changeVideoBladeStorm = BladeStorm.addEventListener('click', () => {
-    if (video.getAttribute('src') !== "/videos/Jett_X_v001_web.mp4"){
-        video.setAttribute('src','/videos/Jett_X_v001_web.mp4')
+    if (video.getAttribute('src') !== "/assets/videos/Jett_X_v001_web.mp4"){
+        video.setAttribute('src','/assets/videos/Jett_X_v001_web.mp4')
     }
 })
+
+
+// Funcion para abrir y cerrar sidenav 
+
+function openSidenav(){
+    const grow = document.getElementById('sidenav');
+    grow.style.width = '35%';
+    document.getElementById('fade').style.width = "100%";
+}
+
+function closeSidenav(){
+    document.getElementById('sidenav').style.width = '0%';
+    document.getElementById('fade').style.width = "0";
+}
