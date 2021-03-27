8b226c401d62fa835809fa98e9c146c3078a4ecd
Jose Sanchez <sanchezjoserafael25@gmail.com>
2021-03-27 19:36:13 -0400

    Se resolvio otro error del responsive en la seccion de skills de los personajes

--- src/components/molecules/Weapons.jsx
+++ src/components/molecules/Weapons.jsx
@@ -1,48 +1,48 @@
 import ListWeapons from "../atoms/ListWeapons";
-import Coins from "../../images/Icons/coins.svg"
-import React from "react"
-import {Link} from "react-router-dom"
+import Coins from "../../images/Icons/coins.svg";
+import React from "react";
+import { Link } from "react-router-dom";
 
 class Weapons extends React.Component {
     render() {
-        return(
+        return (
             <>
-        {ListWeapons[Math.floor(Math.random() * ListWeapons.length)].weapons.map((weapon, index) => (
-            <div className="armas" key={index}>
-                <div className="flex">
-                    <h3 className="armas-title">{weapon.category}</h3>
-                    <Link to={`/Weapons/${weapon.imgLink}`} exact>
-                    <img
-                        className="armas-img"
-                        src={weapon.image}
-                        alt={weapon.name}
-                    />
-                    </Link>
-                </div>
-                <div className="flex">
-                    <p className="armas-text">
-                        <strong>{weapon.name}</strong>
-                    </p>
-                    <div className="icon">
-                        <div>
-                            <img
-                                className="armas-icon"
-                                src={Coins}
-                                alt="points icon"
-                            />
-                            <p className="armas-text-coin">
-                                <strong>{weapon.points}</strong>
+                {ListWeapons[
+                    Math.floor(Math.random() * ListWeapons.length)
+                ].weapons.map((weapon, index) => (
+                    <div className="armas" key={index}>
+                        <div className="flex">
+                            <h3 className="armas-title">{weapon.category}</h3>
+                            <Link to={`/Weapons/${weapon.imgLink}`} exact>
+                                <img
+                                    className="armas-img"
+                                    src={weapon.image}
+                                    alt={weapon.name}
+                                />
+                            </Link>
+                        </div>
+                        <div className="flex">
+                            <p className="armas-text">
+                                <strong>{weapon.name}</strong>
                             </p>
+                            <div className="icon">
+                                <div>
+                                    <img
+                                        className="armas-icon"
+                                        src={Coins}
+                                        alt="points icon"
+                                    />
+                                    <p className="armas-text-coin">
+                                        <strong>{weapon.points}</strong>
+                                    </p>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
-        ))}
-    </>
-        )
+                ))}
+            </>
+        );
     }
-    
 }
-    
 
 export default Weapons;
